f0da3228f5a858bc442f5ab30bba1afc9fa5336a
Tzvi Abel Chajet <achajet@gmail.com>
2020-10-12 20:25:18 -0400

    Got caught up in trying to make this a multiplayer game per the extra credit specifications, might not complete on time.

--- pig_game.py
+++ pig_game.py
@@ -17,17 +17,32 @@ class Pig():
 	def __init__(self,num_players):
 		self.pig_players=[Player(x)for x in range(0,num_players)]
 		self.die=Dice()
-		self.turn(self.pig_players[0])
+		self.turn(self.pig_players)
 	def turn(self,pig_players):
-		while pig_players[0].status==True:
-			roll=self.die.roll()
-			if (roll.score==1):
-				print("You have rolled a 1 and your turn is over")
-				pig_players[0].turn=False
-			else:
-				pig_players[0].current_score=roll
-				pig_players[0].total_score+=roll
-				print("Congratulations Player", str(pig_players[0].num), "your current score is", str(pig_players[0].total_score))
+		for p in range(0,len(pig_players)):
+			roll=self.die
+			while pig_players[p].turn==True:
+				roll.roll()
+				if (roll.score==1):
+					print("Sorry Player", str(pig_players[p].num+1), "You have rolled a 1 and your turn is over")
+					pig_players[p].turn=False
+				else:
+					pig_players[p].current_score=roll.score
+					pig_players[p].total_score+=roll.score
+					print("Congratulations Player", str(pig_players[p].num+1), "you rolled a", str(roll.score), " and your current score is", str(pig_players[p].total_score))
+					choice=input("Would you like to hold or continue to roll? Type h for hold and r to roll")
+					if choice.upper()=="H":
+						pig_players[p].turn=False
+					elif choice.upper()=="R":
+						pig_players[p].turn=True
+					else:
+						print("Not a valid input, please try again")
+						
+				if pig_players[p].total_score==100:
+					print("Congratulations", str(pig_players[p].num+1), "you won!")
+					
+					
+						
 				
 		
 test_roll=Dice()
@@ -36,7 +51,4 @@ test_roll=Dice()
 print(test_roll.score)
 test_roll.roll()
 print(test_roll.score)
-pgame=Pig(5)
-p_one=pgame.pig_players[4]
-
-print(p_one.turn)
+pgame=Pig(2)
